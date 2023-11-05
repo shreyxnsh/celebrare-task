39572153c1de5af02442b3f33500113897baa98f
shreyxnsh <shreyanshatwork@gmail.com>
2023-11-05 12:13:24 +0530

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
-import 'package:celebrare/homepage.dart';
+import 'package:celebrare/HomePage.dart';
+import 'package:celebrare/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    // status bar color
+    statusBarColor: Color(0xff0f837b), 
+  ));
+  
   runApp(const MaterialApp(
     home: Main(),
     debugShowCheckedModeBanner: false,
   ));
+
+  
 }
 
 class Main extends StatelessWidget {
@@ -13,6 +23,6 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Homepage();
+    return const SplashScreen();
   }
 }
